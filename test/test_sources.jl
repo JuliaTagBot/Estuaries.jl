d9dc830f159ab27043c59e6b203725359ecb8d1d
Michael Savastio <Michael.Savastio@VisibleWorld.com>
2017-04-10 14:41:31 -0400

    Testing, changes to gitignore.

--- test/test_sources.jl
+++ test/test_sources.jl
@@ -1,10 +1,19 @@
 using DataTables
 using Estuaries
+using Feather
+using DataUtils
+import DataFrames
 
 const nrows = 10
 
-data = DataTable(A=rand(nrows), B=[randstring(5) for i ∈ 1:nrows])
+function makefeather(filename::String, data::DataTable)
+    Feather.write(filename, convert(DataFrames.DataFrame, data))
+end
 
-E = Estuary(data)
+# data = DataTable(A=rand(nrows), B=[randstring(5) for i ∈ 1:nrows])
+# makefeather("testfile.feather", data)
+
+
+E = Estuary(Feather.Source("testfile.feather"))
 
 
